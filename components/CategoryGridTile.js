c70f6f07b1e08d98fd1973d3912c996fd2510654
Richard Schmidt <richardnew12511@gmail.com>
2022-04-28 13:19:32 -0500

    add color to tiles

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -1,11 +1,13 @@
-import { View, Text, Pressable, StyleSheet } from 'react-native';
+import { View, Text, Pressable, StyleSheet, Platform } from 'react-native';
 
 export default function CategoryGridTile({ title, color }) {
   return (
     <View style={styles.gridItem}>
-      <Pressable style={styles.button}>
-        <View style={styles.innerContainer}>
-          <Text>{title}</Text>
+      <Pressable
+        style={({ pressed }) => [styles.button, pressed ? styles.buttonPressed : null]}
+        android_ripple={{ color: '#ccc' }}>
+        <View style={[styles.innerContainer, { backgroundColor: color }]}>
+          <Text style={styles.title}>{title}</Text>
         </View>
       </Pressable>
     </View>
@@ -24,14 +26,23 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.25,
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 8,
+    overflow: Platform.OS === 'android' ? 'hidden' : 'visible',
   },
   button: {
     flex: 1,
+  },
+  buttonPressed: {
+    opacity: 0.5,
   },
   innerContainer: {
     flex: 1,
     padding: 16,
     justifyContent: 'center',
     alignItems: 'center',
+    borderRadius: 8,
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 18,
   },
 });
