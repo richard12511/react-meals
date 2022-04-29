e55c5d145d549e36391f01d45615936696f75cdd
Richard Schmidt <richardnew12511@gmail.com>
2022-04-29 18:36:11 -0500

    add title

--- screens/MealDetailsScreen.js
+++ screens/MealDetailsScreen.js
@@ -1,9 +1,15 @@
 import { View, Text, StyleSheet } from 'react-native';
 import { MEALS } from '../data/dummy-data';
+import { useLayoutEffect } from 'react';
 
 export default function MealDetailsScreen({ route, navigation }) {
   const id = route.params.id;
   const meal = MEALS.find(meal => meal.id === id);
+
+  useLayoutEffect(() => {
+    navigation.setOptions({ title: meal.title });
+  }, [meal, navigation]);
+
   return (
     <View style={styles.container}>
       <Text>{meal.title}</Text>
